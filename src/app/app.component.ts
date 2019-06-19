679dc8f29cfcadce34f530888d6a7bde7b22d03f
Priyanka Tulshigar <priyanka.tulshigar@globant.com>
2019-06-19 15:25:15 +0530

    unused code removed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,13 +11,13 @@ export class AppComponent {
   g = 0;
   data = this.generateRandomNumber([], 30);
   margin = {
-    top: 20,
-    right: 20,
-    bottom: 30,
+    top: 50,
+    right: 50,
+    bottom: 50,
     left: 50
   };
-  width = 800 - this.margin.left - this.margin.right;
-  height = 400 - this.margin.top - this.margin.bottom;
+  width = window.innerWidth - this.margin.left - this.margin.right;
+  height = window.innerHeight - this.margin.top - this.margin.bottom;
 
   xScale = d3.scaleLinear()
   .range([0, this.width])
@@ -49,132 +49,132 @@ export class AppComponent {
   })
   .entries(this.data);
 
-// 1. Add the SVG to the page and employ #2
-svg = d3.select('body').append('svg')
-.attr('width', this.width + this.margin.left + this.margin.right)
-.attr('height', this.height + this.margin.top + this.margin.bottom);
+  // 1. Add the SVG to the page and employ #2
+  svg = d3.select('body').append('svg')
+  .attr('width', this.width + this.margin.left + this.margin.right)
+  .attr('height', this.height + this.margin.top + this.margin.bottom);
 
-v = this.svg.append('g')
-.attr('class', 'x axis ')
-.attr('id', 'axis--x')
-.attr('transform', 'translate(' + this.margin.left + ',' + (this.height + this.margin.top) + ')')
-.call(this.xAxis);
+  v = this.svg.append('g')
+  .attr('class', 'x axis ')
+  .attr('id', 'axis--x')
+  .attr('transform', 'translate(' + this.margin.left + ',' + (this.height + this.margin.top) + ')')
+  .call(this.xAxis);
 
-b = this.svg.append('g')
-.attr('class', 'y axis')
-.attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')')
-.attr('id', 'axis--y')
-.call(this.yAxis);
+  b = this.svg.append('g')
+  .attr('class', 'y axis')
+  .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')')
+  .attr('id', 'axis--y')
+  .call(this.yAxis);
 
-line = this.svg.append('g').attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')');
+  line = this.svg.append('g').attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')');
 
-dot = this.svg.append('g')
-    .attr('id', 'scatter')
-    .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')');
-
-
-n = this.dataNest.forEach( (d, i) => {
-  // Add line plot
-   this.addLinePlot(d, i);
-}); // End data nest loop
-
-updateInterval = setInterval(() => {
-        this.update();
-      }, 1000);
-
-/****************** Update Below **************************/
+  dot = this.svg.append('g')
+      .attr('id', 'scatter')
+      .attr('transform', 'translate(' + this.margin.left + ',' + this.margin.top + ')');
 
 
-    update() {
+  n = this.dataNest.forEach( (d, i) => {
+    // Add line plot
+    this.addLinePlot(d, i);
+  }); // End data nest loop
 
-      this.generateRandomNumber(this.data, 1);
-      // Nest the entries by name
-      this.dataNest = d3.nest()
-      .key( (d) => {
-          return d.name;
-      })
-      .entries(this.data);
+  // updateInterval = setInterval(() => {
+  //         this.update();
+  //       }, 1000);
 
-      this.xScale.domain(d3.extent(this.data, (d) => {
-        return d.x;
-      })).nice();
+  /****************** Update Below **************************/
 
-      this.yScale.domain(d3.extent(this.data, (d) => {
-        return d.y;
-      })).nice();
 
-      this.yAxis.scale(this.yScale);
-      this.xAxis.scale(this.xScale);
+  update() {
 
-      this.svg.transition().duration(1000).select('.y.axis').call(this.yAxis);
-      this.svg.transition().duration(1000).select('.x.axis').call(this.xAxis);
+    this.generateRandomNumber(this.data, 1);
+    // Nest the entries by name
+    this.dataNest = d3.nest()
+    .key( (d) => {
+        return d.name;
+    })
+    .entries(this.data);
 
-      this.dataNest.forEach( (d, i) => {
+    this.xScale.domain(d3.extent(this.data, (d) => {
+      return d.x;
+    })).nice();
+
+    this.yScale.domain(d3.extent(this.data, (d) => {
+      return d.y;
+    })).nice();
+
+    this.yAxis.scale(this.yScale);
+    this.xAxis.scale(this.xScale);
+
+    this.svg.transition().duration(1000).select('.y.axis').call(this.yAxis);
+    this.svg.transition().duration(1000).select('.x.axis').call(this.xAxis);
+
+    this.dataNest.forEach( (d, i) => {
     if ( d3.select('#line-' + i).empty()) {
             // Add line plot
             this.addLinePlot(d, i);
-      } else {
-          this.line.select('#line-' + i).select('path').data([d.values])
-            .transition().duration(750)
-            .attr('d', this.plotLine);
+    } else {
+        this.line.select('#line-' + i).select('path').data([d.values])
+          .transition().duration(750)
+          .attr('d', this.plotLine);
 
-          // Update all circles
-          this.dot.select('#scatter-' + i).selectAll('circle')
-            .data(d.values)
-            .transition()
-            .duration(750)
+        // Update all circles
+        this.dot.select('#scatter-' + i).selectAll('circle')
+          .data(d.values)
+          .transition()
+          .duration(750)
+          .attr('cx', (d) => {
+            return this.xScale(d.x);
+          })
+          .attr('cy', (d) => {
+            return this.yScale(d.y);
+          })
+          .attr('stroke', 'white')
+          .attr('stroke-width', '2px')
+          .style('fill',  '#ffab00');
+        // Enter new circles
+        this.dot.select('#scatter-' + i).selectAll('circle')
+          .data(d.values)
+          .enter()
+          .append('circle')
             .attr('cx', (d) => {
               return this.xScale(d.x);
             })
             .attr('cy', (d) => {
               return this.yScale(d.y);
             })
+            .attr('r', 5)
             .attr('stroke', 'white')
             .attr('stroke-width', '2px')
             .style('fill',  '#ffab00');
-          // Enter new circles
-          this.dot.select('#scatter-' + i).selectAll('circle')
-            .data(d.values)
-            .enter()
-            .append('circle')
-              .attr('cx', (d) => {
-                return this.xScale(d.x);
-              })
-              .attr('cy', (d) => {
-                return this.yScale(d.y);
-              })
-              .attr('r', 5)
-              .attr('stroke', 'white')
-              .attr('stroke-width', '2px')
-              .style('fill',  '#ffab00');
 
-          // Remove old
-          this.dot.select('#scatter-' + i).selectAll('circle')
-            .data(d.values)
-            .exit()
-            .remove();
-        }
+        // Remove old
+        // this.dot.select('#scatter-' + i).selectAll('circle')
+        //   .data(d.values)
+        //   .exit()
+        //   .remove();
+      }
     });
 
-    }
+  }
 
-    generateRandomNumber(data, samples) {
-      for (let i = 0; i < samples; i++) {
-        data.push({
-          x: this.g,
-          y: Math.sin(Math.random()),
-          name: 'group-1'
-        });
-        this.g++;
-      }
-      data.sort((a, b) => {
-        return a.x - b.x;
+  generateRandomNumber(data, samples) {
+    for (let i = 0; i < samples; i++) {
+      data.push({
+        x: this.g,
+        y: Math.sin(Math.random()),
+        name: 'group-1'
       });
-      return data;
+      this.g++;
     }
+    data.sort((a, b) => {
+      return a.x - b.x;
+    });
+    return data;
+  }
 
-    addLinePlot(d, i) {
-      this.line.append('g')
+  addLinePlot(d, i) {
+    this.line.append('g')
       .attr('id', 'line-' + i)
       .attr('clip-path', 'url(#clip)')
       .append('path')
@@ -184,7 +184,7 @@ updateInterval = setInterval(() => {
       .style('fill', 'none')
       .style('stroke',  '#ffab00');
 
-      this.dot.append('g')
+    this.dot.append('g')
       .attr('id', 'scatter-' + i)
       .attr('clip-path', 'url(#clip)')
       .selectAll('.dot')
@@ -201,6 +201,6 @@ updateInterval = setInterval(() => {
       .attr('stroke', 'white')
       .attr('stroke-width', '2px')
       .style('fill',  '#ffab00');
-    }
+  }
 }
 
